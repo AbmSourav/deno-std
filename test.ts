97c2805fa0a5c86b6f95a2e04b476edf54b4f3e8
Keramot UL Islam <keramotul.islam@gmail.com>
2020-05-17 20:30:28 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,7 +2,4 @@ import { serve } from "https://deno.land/std/http/server.ts";
 
 export function serverTest() {
   const createServer = serve({ port: 8080 });
-  for await (const req of createServer) {
-    req.respond({ body: "Hello Universe \n" + req.url });
-  }
 }
