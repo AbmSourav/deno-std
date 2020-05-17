7dbbcafe55558980e0a872b737434cc812b59c58
Keramot UL Islam <keramotul.islam@gmail.com>
2020-05-17 21:41:02 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
 
-export function serverTest(options, body) {
+export function serverTest(options: object, body: string) {
   return listenAndServe(options, (req) => {
     req.respond({ body });
   });
